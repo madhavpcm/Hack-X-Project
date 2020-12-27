32da3ee90b6dbf353cb33f363ec238a8ab661587
madhavpcm <dpmadhav@hotmail.com>
2020-12-27 22:13:46 +0530

    All features working

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 import flask
+import requests
+import json
 from flask import request, url_for, render_template, redirect
 
 
 app = flask.Flask(__name__)
 
+mapbox_access_token = 'pk.eyJ1IjoibWFkaGF2cGNtIiwiYSI6ImNrajVqcGZ3MTYxYmozMnFqa3ZwYjQyOTcifQ.kYiYDhvTs1VOeSHWvliCAg' #mapbox api
+URL = "https://geocode.search.hereapi.com/v1/geocode"
+api_key = '3oCmlgIcmiyNN3Mtpxl4gpo2HxzHASIhNZYe1A-sqVU' #hereapi
+
 @app.route('/',methods=['GET','POST'])
 def my_maps():
-
-    mapbox_access_token = '' #put ur id here
-    
     return render_template('index.html',
         mapbox_access_token=mapbox_access_token)
 
+@app.route('/getmap')
+def renderlocation():
+    name=request.args.get('place') 
+    PARAMS = {'apikey':api_key,'q':name} 
+    if name:
+        print(name) 
+        r = requests.get(url = URL, params = PARAMS) 
+        data = r.json()
+ 
+        latitude = data['items'][0]['position']['lat']
+        longitude = data['items'][0]['position']['lng']
+
+        return render_template('greet.html',mapbox_access_token=mapbox_access_token , latitude = latitude, longitude = longitude)
+    else:
+        return render_template('index.html',
+            mapbox_access_token=mapbox_access_token)
 
 
